ee3b636d50dfd73b2b5d11f7a5865e78cb9d5f9c
alicevalves <73171191+alicevalves@users.noreply.github.com>
2021-11-20 16:58:12 -0300

    [v3.0.0] Implementação da Classe Usuário

--- Irriga Vida - Sistema de Irrigacao/Sistema - Irriga Vida - PI - ADS - 3/src/metodos_projeto/Usuario.java
+++ Irriga Vida - Sistema de Irrigacao/Sistema - Irriga Vida - PI - ADS - 3/src/metodos_projeto/Usuario.java
@@ -8,79 +8,173 @@ public class Usuario {
 	private String email;
 	private String status;
 	private String perfil;
+	private String dtCadastro;
+	private String dtAlteracao;
 	
-	
-	public Usuario(String text, String text2, String text3, String text4, String statusUsuario, String statusPerfil) {
+	/**
+	 * Método Construtor para receber o código do Usuário.
+	 * @param text - variável que armazena o valor recebido do ID.
+	 */
+	public Usuario(String text) {
+		this.id= text;
+	}
+
+
+	/**
+	 * Método Construtor para receber todos os dados de um Usuário.
+	 * @param text - variável que armazena o valor recebido do ID.
+	 * @param text2 - variável que armazena o valor recebido do NOME.
+	 * @param text3 - variável que armazena o valor recebido da SENHA.
+	 * @param dtCadastro - variável que armazena o valor recebido da DATA DE CADASTRO.
+	 * @param text4 - variável que armazena o valor recebido dO EMAIL.
+	 * @param dtAlteracao - variável que armazena o valor recebido da DATA DE ALTERAÇÃO.
+	 * @param statusUsuario - variável que armazena o valor recebido do STATUS DO USUÁRIO.
+	 * @param statusPerfil - variável que armazena o valor recebido do PERFIL.
+	 */
+	public Usuario(String text, String text2, String text3, String dtCadastro, String text4, String dtAlteracao,
+			String statusUsuario, String statusPerfil) {
 		this.id= text;
 		this.nome=text2;
+		this.dtCadastro=dtCadastro;
 		this.senha=text3;
 		this.email=text4;
+		this.dtAlteracao=dtAlteracao;
 		this.status=statusUsuario;
 		this.perfil=statusPerfil;
 	}
 
 
+	/**
+	 * Método que recebe a DATA DE CADASTRO.
+	 * @return - retorna a DATA DE CADASTRO.
+	 */
+	public String getDtCadastro() {
+		return dtCadastro;
+	}
+
+	/**
+	 * Método que define a DATA DE CADASTRO.
+	 * @param dtCadastro - variável que armazena a DATA DE CADASTRO.
+	 */
+	public void setDtCadastro(String dtCadastro) {
+		this.dtCadastro = dtCadastro;
+	}
+
+
+	/**
+	 * Método que recebe a DATA DE ALTERAÇÃO.
+	 * @return - retorna a DATA DE ALTERAÇÃO.
+	 */
+	public String getDtAlteracao() {
+		return dtAlteracao;
+	}
+
+
+	/**
+	 * Método que define a DATA DE ALTERAÇÃO.
+	 * @param dtAlteracao - variável que armazena a DATA DE ALTERAÇÃO.
+	 */
+	public void setDtAlteracao(String dtAlteracao) {
+		this.dtAlteracao = dtAlteracao;
+	}
+
+
+	/**
+	 * Método que recebe o ID.
+	 * @return - retorna o ID.
+	 */
 	public String getId() {
 		return id;
 	}
 
 
-	public void setId(String id) {
-		this.id = id;
+	/**
+	 * Método que define o ID.
+	 * @param text - variável que armazena o ID.
+	 */
+	public void setId(String text) {
+		this.id = text;
 	}
 
 
+	/**
+	 * Método que recebe o NOME.
+	 * @return - retorna o NOME.
+	 */
 	public String getNome() {
 		return nome;
 	}
 
-
-	public void setNome(String nome) {
-		this.nome = nome;
+	/**
+	 * Método que define o NOME.
+	 * @param text2 - variável que armazena o NOME.
+	 */
+	public void setNome(String text2) {
+		this.nome = text2;
 	}
 
-
+	/**
+	 * Método que recebe a SENHA.
+	 * @return - retorna a SENHA.
+	 */
 	public String getSenha() {
 		return senha;
 	}
 
-
-	public void setSenha(String senha) {
-		this.senha = senha;
+	/**
+	 * Método que define a SENHA.
+	 * @param text3 - variável que armazena a SENHA.
+	 */
+	public void setSenha(String text3) {
+		this.senha = text3;
 	}
 
 
+	/**
+	 * Método que recebe o EMAIL.
+	 * @return - retorna o EMAIL.
+	 */
 	public String getEmail() {
 		return email;
 	}
 
-
-	public void setEmail(String email) {
-		this.email = email;
+	/**
+	 * Método que define o EMAIL.
+	 * @param text4 - variável que armazena o EMAIL.
+	 */
+	public void setEmail(String text4) {
+		this.email = text4;
 	}
 
-
+	/**
+	 * Método que recebe o STATUS.
+	 * @return - retorna o STATUS.
+	 */
 	public String getStatus() {
 		return status;
 	}
 
-
-	public void setStatus(String status) {
-		this.status = status;
+	/**
+	 * Método que define o STATUS.
+	 * @param statusUsuario - variável que armazena o STATUS.
+	 */
+	public void setStatus(String statusUsuario) {
+		this.status = statusUsuario;
 	}
 
-
+	/**
+	 * Método que recebe o PERFIL.
+	 * @return - retorna o PERFIL.
+	 */
 	public String getPerfil() {
 		return perfil;
 	}
 
-
-	public void setPerfil(String perfil) {
-		this.perfil = perfil;
+	/**
+	 * Método que define o PERFIL.
+	 * @param statusPerfil - variável que armazena o PERFIL.
+	 */
+	public void setPerfil(String statusPerfil) {
+		this.perfil = statusPerfil;
 	}
-
-
-	
-	
-	
 }
